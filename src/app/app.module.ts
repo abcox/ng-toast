401d027fd7c7f5698aa245dbb7207491ffe75b25
Adam Cox <Adam@ADAM>
2018-08-18 22:18:38 -0400

    import material modules via separate module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 
 // Material Design
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material-module';
 
 // Application
 import { AppComponent } from './app.component';
@@ -15,6 +16,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule, // Material Design Animation
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
