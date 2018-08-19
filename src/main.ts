7857d52ef6a732e3c392a307a94570f2ddccdb6d
Adam Cox <Adam@ADAM>
2018-08-18 23:18:24 -0400

    add gesture support via hammerjs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+// Gesture support via hammerjs
+import 'hammerjs';
+
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
